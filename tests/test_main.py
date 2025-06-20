a5cf3e87803a642581dc45a0c05ab8ec99762bda
Sphesihle Makhathini <sphemakh@gmail.com>
2025-06-20 11:48:45 +0200

    start pytest

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+import pytest
+from contsub.main import add
+
+def test_add_positive_numbers():
+    assert add(2, 3) == 5
+
+def test_add_negative_numbers():
+    assert add(-2, -3) == -5
+
+def test_add_zero():
+    assert add(0, 0) == 0
